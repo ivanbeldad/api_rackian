0cf3c46245da3265b29ea24a4c844228a3191600
Ivan de la Beldad <ivandelabeldad@gmail.com>
2017-04-16 22:19:39 +0100

    Customized user model. Added new id. Added space field. Created view and serializer for users.

--- api_rackian/settings.py
+++ api_rackian/settings.py
@@ -22,6 +22,8 @@ DATABASES = local_settings.DATABASES
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+AUTH_USER_MODEL = 'authentication.User'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
