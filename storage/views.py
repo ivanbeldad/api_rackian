9e4757904becfe897627e83e10a07fd6c193031d
Ivan de la Beldad <ivandelabeldad@gmail.com>
2017-04-16 22:55:26 +0100

    Added limit to uploads. Create file now increase the user size. Destroy file now decrease the user size.

--- storage/views.py
+++ storage/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse
 from api_rackian import settings
 from rest_framework import viewsets, views
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from storage.models import Folder, File
 from storage.serializers import FolderSerializer, FileSerializer
-from rest_framework import authentication, permissions, parsers
+from rest_framework import authentication, permissions, parsers, status
 import mimetypes
 
 
@@ -39,6 +38,35 @@ class FileViewSet(viewsets.ModelViewSet):
             else:
                 files = files.filter(folder=None)
         return files
+
+    @staticmethod
+    def max_space(request):
+        # 100MB
+        return 104857600
+
+    @staticmethod
+    def have_space(request):
+        max_space = FileViewSet.max_space(request)
+        space = request.user.space
+        file_space = request.data['link'].size
+        return (space + file_space) <= max_space
+
+    def create(self, request, *args, **kwargs):
+        if not FileViewSet.have_space(self.request):
+            data = {'error': 'Not enough space'}
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+        user = self.request.user
+        user.space = user.space + request.data['link'].size
+        user.save()
+        return super(FileViewSet, self).create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        user = self.request.user
+        user.space = user.space - self.get_object().size
+        if user.space < 0:
+            user.space = 0
+        user.save()
+        return super(FileViewSet, self).destroy(request, *args, **kwargs)
 
 
 class DownloadableFileView(views.APIView):
